2578171248f8983321867b4a74dc5daaa19de309
chungyen100 <chang_chungyen@yahoo.com>
2015-05-04 16:00:38 -0700

    Added mpstudy.sh

--- mpstudy.sh
+++ mpstudy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+experimentName="experiment1";
 
 i=0;
-while [ $i -lt 2 ]; do
+while [ $i -lt 3 ]; do
+	ofile=${experimentName}_$i.txt;
 	killall mongod
+	sleep 10
 	rm -r ~/data/testdb
 	mkdir -p ~/data/testdb
 	# ~/scripts/known_state.sh
@@ -10,7 +13,10 @@ while [ $i -lt 2 ]; do
 	for f in $( ls testcases/*.js ); do
 		fname=${f%.js}
 		fname=${fname##*/}
-		time numactl --interleave=all python ~/mongo-perf/benchrun.py -f $f --writeCmd true --nodyno --mongo-repo-path ~/mongo/bin/mongo-master -s ~/mongo/bin/mongo -l mptest${i}_$fname --rport 27117 -t 1 4 8 16 32 64 96 --trialCount 7 --testFilter "['sanity','daily']" | tee -a mp.txt
+		# echo "Starting test $fname at $(date)" | tee -a $ofile
+		(time numactl --interleave=all python ~/mongo-perf/benchrun.py -f $f --writeCmd true --nodyno --mongo-repo-path ~/mongo/bin/mongo-master -s ~/mongo/bin/mongo -l ${experimentName}_${i}_$fname --rport 27117 -t 1 4 8 16 32 64 96 --trialCount 7 --testFilter "['sanity','daily']") 2>&1 | tee -a $ofile
+		#(time numactl --interleave=all python ~/mongo-perf/benchrun.py -f $f --writeCmd true --nodyno --mongo-repo-path ~/mongo/bin/mongo-master -s ~/mongo/bin/mongo -l mptest${i}_$fname --rport 27117 -t 1 4 8 16 32 64 96 --trialCount 7 --testFilter "['sanity','daily']") | tee -a $ofile
+		printf "\n\n\n\n" | tee -a $ofile 
 	done
 	i=$(($i+1))
 done

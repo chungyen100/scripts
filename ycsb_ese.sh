2f6ace7a300eb6d67ebc2cede359f4d6bf700c24
EC2 Default User <ec2-user@ip-10-3-1-99.ec2.internal>
2015-08-10 01:46:17 +0000

    Modified ycsb_ese.sh

--- ycsb_ese.sh
+++ ycsb_ese.sh
@@ -10,13 +10,12 @@ while [ $i -lt 1 ]; do
 	rm -r ~/data/testdb
 	mkdir -p ~/data/testdb
 	~/scripts/known_state.sh
-	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mptest.log --storageEngine wiredTiger --fork
-# numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mptest.log --storageEngine wiredTiger --enableEncryption --encryptionKeyFile ~/ese.key --fork
+#	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mptest.log --storageEngine wiredTiger --fork
+    numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mongod.log --storageEngine wiredTiger --enableEncryption --encryptionKeyFile ~/ese.key --fork
     sleep 10
-    mkdir ese
-    cd ese
-    ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -P ~/YCSB/ycsb-mongodb/workloads/workload_ese
-    ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -P ~/YCSB/ycsb-mongodb/workloads/workload_ese
+    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee ese_load.txt
+    sleep 10
+    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee ese_run.txt
     
     # Now do the baseline 
     ofile=wt_${experimentName}_baseline_$i.txt;
@@ -25,10 +24,11 @@ while [ $i -lt 1 ]; do
 	rm -r ~/data/testdb
 	mkdir -p ~/data/testdb
 	~/scripts/known_state.sh
-	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mptest.log --storageEngine wiredTiger --fork
+	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mongod.log --storageEngine wiredTiger --fork
     sleep 10
-    ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -P ~/YCSB/ycsb-mongodb/workloads/workload_ese
-    ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -P ~/YCSB/ycsb-mongodb/workloads/workload_ese
+    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee base_load.txt
+    leep 10
+    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee base_run.txt
     
     i=$(($i+1))
 done

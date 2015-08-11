c8067a809565e356ec0c943fe8a9e19f52d7a201
EC2 Default User <ec2-user@ip-10-2-1-226.ec2.internal>
2015-08-11 07:46:54 +0000

    Modified ycsb_ese.sh

--- ycsb_ese.sh
+++ ycsb_ese.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-experimentName="ese_84182ff";
 
 i=0;
 while [ $i -lt 1 ]; do
     # Let's start with the encrypted tests
-    ofile=wt_${experimentName}_$i.txt;
-	killall mongod
-	sleep 10
-	rm -r ~/data/testdb
-	mkdir -p ~/data/testdb
-	~/scripts/known_state.sh
-#	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mptest.log --storageEngine wiredTiger --fork
+    killall mongod
+    sleep 10
+    rm -rf ~/data/testdb
+    mkdir -p ~/data/testdb
+    ~/scripts/known_state.sh
     numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mongod.log --storageEngine wiredTiger --enableEncryption --encryptionKeyFile ~/ese.key --fork
     sleep 10
-    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee ese_load.txt
-    sleep 10
-    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee ese_run.txt
-    
+    (stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese -threads 16 2>&1 | tee ese_load.txt) &
+    sleep 30
+    ~/scripts/monstat.sh ese_load 1 300
+    wait
+    (stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese -threads 16 2>&1 | tee ese_run.txt) &
+    sleep 30
+    ~/scripts/monstat.sh ese_run 1 300
+    wait
+
     # Now do the baseline 
-    ofile=wt_${experimentName}_baseline_$i.txt;
-	killall mongod
-	sleep 10
-	rm -r ~/data/testdb
-	mkdir -p ~/data/testdb
-	~/scripts/known_state.sh
-	numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mongod.log --storageEngine wiredTiger --fork
+    killall mongod
     sleep 10
-    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee base_load.txt
-    leep 10
-    stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese 2>&1 | tee base_run.txt
+    rm -rf ~/data/testdb
+    mkdir -p ~/data/testdb
+    ~/scripts/known_state.sh
+    numactl --interleave=all ~/mongo/bin/mongod --dbpath ~/data/testdb --logpath ~/data/testdb/mongod.log --storageEngine wiredTiger --fork
+    sleep 10
+    (stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb load mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese -threads 16 2>&1 | tee base_load.txt) &
+    sleep 30
+    ~/scripts/monstat.sh base_load 1 300
+    wait 
+    (stdbuf -o L ~/YCSB/ycsb-mongodb/bin/ycsb run mongodb -s -P ~/YCSB/ycsb-mongodb/workloads/workload_ese -threads 16 2>&1 | tee base_run.txt) &
+    sleep 30 
+    ~/scripts/monstat.sh base_run 1 300
+    wait
     
     i=$(($i+1))
 done

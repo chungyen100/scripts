9fea08ae7eb43112bd2f34b0773b281bda6b544d
EC2 Default User <ec2-user@ip-10-3-1-98.ec2.internal>
2015-08-27 23:12:34 +0000

    Modified monstat.sh to allow remote data collection

--- monstat.sh
+++ monstat.sh
@@ -1,20 +1,21 @@
-#!/bin/bash                                                                                                                                 # data collection using support's timeseries tool 
+#!/bin/bash -x                                                                                                                               # data collection using support's timeseries tool :u
+
+# this is a quick hack to do remote collection
+# need to get back and clean up with single node tasks
 
 TNAME=$1
 __delay=$2
 __duration=$3
-
-MONGO="~/mongo/bin/mongo"
+__host=$4
 
 bg_procs=""
 
-eval "$MONGO --eval 'while(true) {print(JSON.stringify(db.serverStatus())); sleep(1000*$__delay)}'" > ss_$TNAME.log &
+mongo --eval "while(true) {print(JSON.stringify(db.serverStatus())); sleep(1000*${__delay})}" ${__host} > ss_$TNAME.${__host}.log &
 bg_procs="$! $bg_procs"
-iostat -k -t -x ${__delay} > iostat_$TNAME.log &
+ssh ec2-user@${__host} "iostat -k -t -x ${__delay}" > iostat_$TNAME.${__host}.log &
 bg_procs="$! $bg_procs"
-python ~/scripts/sysmon.py ${__delay} > sysmon_$TNAME.log &
+cat ~/scripts/sysmon.py | ssh ec2-user@${__host} python - ${__delay} > sysmon_$TNAME.${__host}.log &
 bg_procs="$! $bg_procs"
-
 
 sleep ${__duration}
 trap 'kill $bg_procs' EXIT
---
+++ monstat.sh.single
@@ -0,0 +1,25 @@
+#!/bin/bash                                                                                                                                 # data collection using support's timeseries tool 
+
+TNAME=$1
+__delay=$2
+__duration=$3
+--host=$4
+
+MONGO="~/mongo/bin/mongo"
+
+bg_procs=""
+
+eval "$MONGO --eval 'while(true) {print(JSON.stringify(db.serverStatus())); sleep(1000*$__delay)}'" > ss_$TNAME.log &
+bg_procs="$! $bg_procs"
+iostat -k -t -x ${__delay} > iostat_$TNAME.log &
+bg_procs="$! $bg_procs"
+python ~/scripts/sysmon.py ${__delay} > sysmon_$TNAME.log &
+bg_procs="$! $bg_procs"
+
+
+sleep ${__duration}
+trap 'kill $bg_procs' EXIT
+
+
+
+
